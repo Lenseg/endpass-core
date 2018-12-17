a5a8b34c36f7f6beca9dbe466fe4bbfaae1e530c
Mona Lisa <Lenseg1@gmail.com>
2018-12-17 06:20:35 +0300

    utils to repo

---
+++ packages/utils/jest.conf.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const ENV = require('./test.env');
+
+module.exports = {
+  rootDir: './',
+  moduleFileExtensions: ['js', 'json', 'vue', 'ts', 'tsx', 'jsx', 'node'],
+  testMatch: ['<rootDir>/**/__tests__/*.js'],
+
+  globals: {
+    ENV,
+  },
+  transform: {
+    '^.+\\.js$':'babel-jest',
+  },
+};
